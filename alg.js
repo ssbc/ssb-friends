2affdfef41f85d3c3c3fc46bd45a907232801531
Dominic Tarr <dominic.tarr@gmail.com>
2017-06-01 09:59:43 +1200

    shift all the friends stuff in here

---
+++ alg.js
@@ -0,0 +1,77 @@
+var G = require('graphreduce')
+
+exports.add = function (graph, from, to, value) {
+  if(value === null)
+    return G.removeEdge(graph, from, to), graph
+  else
+    return G.addEdge(graph, from, to, value)
+}
+
+function min (a, b) {
+  return Math.min(
+    a || Number.POSITIVE_INFINITY,
+    b || Number.POSITIVE_INFINITY
+  )
+}
+
+var defaultOpts = {
+  initial: 0,
+  compare: function compare (a, b) {
+    return a < b ? -1 : a > b ? 1 : 0
+  },
+  update: function (then, now) {
+    return then == null && now != null || then > now
+  },
+  reduce: function reduce (target, source, value) {
+    return min(target,
+        value === false ? ~source
+      : value === true ? source + 1
+      : target
+    )
+    return target
+  },
+  expand: function expand (v) {
+    return v != null && v >= 0 && v <= 3
+  }
+}
+
+
+exports.reachable = function (graph, start, opts) {
+  if(!opts)
+    opts = defaultOpts
+
+  var visited = {}
+  visited[start] = opts.initial
+  var queue = [start]
+  while(queue.length) {
+    var cursor = queue.shift()
+    var value = visited[cursor]
+    for(var k in graph[cursor]) {
+      var v = graph[cursor][k]
+      var previous = visited[k]
+      var value = opts.reduce(visited[k], visited[cursor], v)
+      if(value != null) {
+        visited[k] = value
+      }
+      if(opts.expand(visited[k]) && previous == null)
+          queue.push(k)
+    }
+  }
+  return visited
+}
+
+exports.diff = function (then, now, opts) {
+  if(!opts)
+    opts = defaultOpts
+
+  var added = {}
+  for(var k in now) {
+    if(then[k] == null || opts.update(then[k], now[k]))
+      added[k] = now[k]
+  }
+
+  for(var k in then) {
+    if(now[k] == null) added[k] = null
+  }
+  return added
+}
--- index.js
+++ index.js
@@ -1,77 +1,206 @@
-var G = require('graphreduce')
+var G           = require('graphreduce')
+var Reduce      = require('flumeview-reduce')
+var pull        = require('pull-stream')
+var FlatMap     = require('pull-flatmap')
+//var mlib        = require('ssb-msgs')
+//var pushable    = require('pull-pushable')
+//var mdm         = require('mdmanifest')
+//var valid       = require('../lib/validators')
+//var apidoc      = require('../lib/apidocs').friends
+var ref         = require('ssb-ref')
+var Obv         = require('obv')
 
-exports.add = function (graph, from, to, value) {
-  if(value === null)
-    return G.removeEdge(graph, from, to), graph
-  else
-    return G.addEdge(graph, from, to, value)
+var F           = require('./alg')
+var block       = require('./block')
+
+// friends plugin
+// methods to analyze the social graph
+// maintains a 'follow' and 'flag' graph
+
+function isFunction (f) {
+  return 'function' === typeof f
 }
 
-function min (a, b) {
-  return Math.min(
-    a || Number.POSITIVE_INFINITY,
-    b || Number.POSITIVE_INFINITY
-  )
+function isString (s) {
+  return 'string' === typeof s
 }
 
-var defaultOpts = {
-  initial: 0,
-  compare: function compare (a, b) {
-    return a < b ? -1 : a > b ? 1 : 0
-  },
-  update: function (then, now) {
-    return then == null && now != null || then > now
-  },
-  reduce: function reduce (target, source, value) {
-    return min(target,
-        value === false ? ~source
-      : value === true ? source + 1
-      : target
-    )
-    return target
-  },
-  expand: function expand (v) {
-    return v != null && v >= 0 && v <= 3
-  }
+function isFriend (friends, a, b) {
+  return friends[a] && friends[b] && friends[a][b] && friends[b][a]
 }
 
+exports.name = 'friends'
+exports.version = '1.0.0'
+exports.manifest = {
+  get: 'async',
+  createFriendStream: 'source',
+  hops: 'async'
+}
 
-exports.reachable = function (graph, start, opts) {
-  if(!opts)
-    opts = defaultOpts
+//mdm.manifest(apidoc)
 
-  var visited = {}
-  visited[start] = opts.initial
-  var queue = [start]
-  while(queue.length) {
-    var cursor = queue.shift()
-    var value = visited[cursor]
-    for(var k in graph[cursor]) {
-      var v = graph[cursor][k]
-      var previous = visited[k]
-      var value = opts.reduce(visited[k], visited[cursor], v)
-      if(value != null) {
-        visited[k] = value
+exports.init = function (sbot, config) {
+  var post = Obv()
+  post.set({})
+  var index = sbot._flumeUse('friends', Reduce(2, function (g, rel) {
+    if(!g) g = {}
+    G.addEdge(g, rel.from, rel.to, rel.value)
+    return g
+  }, function (data) {
+    if(data.value.content.type === 'contact' && ref.isFeed(data.value.content.contact)) {
+      var tristate = (
+        data.value.content.following ? true
+      : data.value.content.flagged || data.value.content.blocking ? false
+      : null
+      )
+      return {
+        from: data.value.author,
+        to: data.value.content.contact,
+        value: tristate
       }
-      if(opts.expand(visited[k]) && previous == null)
-          queue.push(k)
+    }
+  }))
+
+  function createFriendStream (opts) {
+    opts = opts || {}
+    var live = opts.live === true
+    var meta = opts.meta === true
+    var start = opts.start || sbot.id
+    var reachable
+    //mabye this could be rewritten cleaner with
+    //index.value (an observable) instead?
+    return pull(
+      index.stream(opts),
+      FlatMap(function (v) {
+        if(!v) return []
+
+        //this code handles real time streaming of the hops map.
+        function push (to, hops) {
+          out.push(meta ? {id: to, hops: hops} : to)
+        }
+
+        var out = [], g = post.value
+
+        //the edge has already been added to g
+        if(!reachable) {
+          reachable = F.reachable(g, start, block)
+          for(var k in reachable)
+            if(block.isWanted(reachable[k]))
+              push(k, reachable[k][0])
+        } else {
+          var _reachable = F.reachable(g, start, block)
+          var patch = F.diff(reachable, _reachable, block)
+          for(var k in patch) {
+            if(patch[k] == null || patch[k][0] == null || patch[k][0] > patch[k][1])
+              push(k, -1)
+            else if(block.isWanted(patch[k]))
+              push(k, patch[k][0])
+          }
+          reachable = _reachable
+        }
+        return out
+      })
+
+    )
+  }
+
+
+  //BLOCKING
+
+  var g = {}
+
+  post(function (_g) {
+    g = _g
+  })
+
+  function isBlocked (_opts) {
+    var opts
+    if(!g) return //only possible briefly at startup
+    if('string' === typeof _opts)
+      return g[sbot.id] ? g[sbot.id][_opts] === false : false
+    return g[_opts.source] ? g[_opts.source][_opts.dest] === false : false
+  }
+
+  //should things like createHistoryStream instead
+  //call a block prehook?
+  sbot.createHistoryStream.hook(function (fn, args) {
+    var opts = args[0], id = this.id
+    //reminder: this.id is the remote caller.
+    if(opts.id !== this.id && isBlocked({source: opts.id, dest: id})) {
+      return function (abort, cb) {
+        //just give them the cold shoulder
+      }
+//      return fn({id: null, sequence: 0})
+    } else
+      return pull(
+        fn.apply(this, args),
+        //break off this feed if they suddenly block
+        //the recipient.
+        pull.take(function (msg) {
+          //handle when createHistoryStream is called with keys: true
+          if(!msg.content && msg.value.content)
+            msg = msg.value
+          if(msg.content.type !== 'contact') return true
+          return !(
+            (msg.content.flagged || msg.content.blocking) &&
+            msg.content.contact === id
+          )
+        })
+      )
+  })
+
+  sbot.auth.hook(function (fn, args) {
+    if(isBlocked(args[0])) args[1](new Error('client is blocked'))
+    else return fn.apply(this, args)
+  })
+
+  // ^^ BLOCKING
+
+  // REPLICATION
+  pull(
+    createFriendStream({live: true, meta: true}),
+    // filter out duplicates, and also keep track of what we expect to receive
+    // lookup the latest sequence from each user
+    // TODO: use paramap?
+    pull.drain(function (data) {
+      if(data.sync) return
+      if(data.hops >= 0)
+        sbot.replicate.request(data.id)
+      else
+        sbot.replicate.request(data.id, false)
+    })
+  )
+  //
+
+
+  index.since(function () {
+    //it looks async but this will always be sync after loading
+    index.get(null, function (_, v) {
+      post.set(v)
+    })
+  })
+
+  return {
+    post: post,
+    get: function (opts, cb) {
+      index.get(opts, cb)
+    },
+
+    createFriendStream: createFriendStream,
+    //legacy, debugging
+    hops: function (opts, cb) {
+      if(isFunction(opts))
+        cb = opts, opts = {}
+      opts = opts || {}
+      if(isString(opts))
+        opts = {start: opts}
+      index.get(null, function (err, g) {
+        if(err) cb(err)
+        else cb(null, G.hops(g, opts.start || sbot.id, 0, opts.hops || 3))
+      })
     }
   }
-  return visited
 }
 
-exports.diff = function (then, now, opts) {
-  if(!opts)
-    opts = defaultOpts
 
-  var added = {}
-  for(var k in now) {
-    if(then[k] == null || opts.update(then[k], now[k]))
-      added[k] = now[k]
-  }
 
-  for(var k in then) {
-    if(now[k] == null) added[k] = null
-  }
-  return added
-}
