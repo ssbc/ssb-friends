fbe8309fe5c7d34833c35b7e55fdf1ed365821ea
Anders Rune Jensen <arj03@protonmail.ch>
2021-02-16 09:42:06 +0100

    Support new db2 index format

--- db2-contacts.js
+++ db2-contacts.js
@@ -6,197 +6,167 @@ const Plugin = require('ssb-db2/indexes/plugin')
 
 const isFeed = require('ssb-ref').isFeed
 
-module.exports = function (sbot, createLayer, config) {
-  const layer = createLayer('contacts')
+const bValue = Buffer.from('value')
+const bAuthor = Buffer.from('author')
+const bContent = Buffer.from('content')
+const bType = Buffer.from('type')
+const bContact = Buffer.from('contact')
 
-  // used for dictionary compression where a feed is mapped to its index
-  let feeds = []
+module.exports = function(createLayer) {
+  return class Friends extends Plugin {
+    constructor(log, dir) {
+      super(log, dir, 'contacts', 2, undefined, 'json')
+      this.layer = createLayer('contacts')
+      this.layer({})
 
-  // a map of feed -> { feed: followStatus }
-  const feedValues = {}
-  // assuming we have feed A (index 0) and B, and A follows B we will in feedValues store:
-  // { 0: { 1: 1 } } meaning the map of values for feed A (0) is: index 1 (B) has value 1 (follow)
-  //
-  // feeds will be: [A,B] in this example
+      // used for dictionary compression where a feed is mapped to its index
+      this.feeds = []
 
-  const bValue = Buffer.from('value')
-  const bAuthor = Buffer.from('author')
-  const bContent = Buffer.from('content')
-  const bType = Buffer.from('type')
-  const bContact = Buffer.from('contact')
+      // a map of feed -> { feed: followStatus }
+      this.feedValues = {}
+      // assuming we have feed A (index 0) and B, and A follows B we will in feedValues store:
+      // { 0: { 1: 1 } } meaning the map of values for feed A (0) is: index 1 (B) has value 1 (follow)
+      //
+      // feeds will be: [A,B] in this example
 
-  const name = 'contacts'
-  const { level, offset, stateLoaded, onData, writeBatch } = Plugin(
-    config.path,
-    name,
-    2,
-    handleData,
-    writeData,
-    beforeIndexUpdate
-  )
+      // it turns out that if you place the same key in a batch multiple
+      // times. Level will happily write that key as many times as you give
+      // it, instead of just writing the last value for the key, so we have
+      // to help the poor bugger
+      this.batchKeys = {} // key to index
+    }
 
-  let batch = []
-  // it turns out that if you place the same key in a batch multiple
-  // times. Level will happily write that key as many times as you give
-  // it, instead of just writing the last value for the key, so we have
-  // to help the poor bugger
-  let batchKeys = {} // key to index
+    onFlush(cb) {
+      this.batchKeys = {}
+      cb()
+    }
 
-  function writeData(cb) {
-    level.batch(batch, { valueEncoding: 'json' }, cb)
-    batch = []
-    batchKeys = {}
-  }
+    processRecord(record, seq) {
+      const recBuffer = record.value
+      if (!recBuffer) return // deleted
 
-  function handleData(record, processed) {
-    if (record.offset < offset.value) return batch.length
-    const recBuffer = record.value
-    if (!recBuffer) return batch.length // deleted
+      let p = 0 // note you pass in p!
+      p = bipf.seekKey(recBuffer, p, bValue)
+      if (p < 0) return
 
-    let p = 0 // note you pass in p!
-    p = bipf.seekKey(recBuffer, p, bValue)
-    if (!~p) return batch.length
+      const pAuthor = bipf.seekKey(recBuffer, p, bAuthor)
+      const author = bipf.decode(recBuffer, pAuthor)
 
-    const pAuthor = bipf.seekKey(recBuffer, p, bAuthor)
-    const author = bipf.decode(recBuffer, pAuthor)
+      const pContent = bipf.seekKey(recBuffer, p, bContent)
+      if (pContent < 0) return
 
-    const pContent = bipf.seekKey(recBuffer, p, bContent)
-    if (!~pContent) return batch.length
+      const pType = bipf.seekKey(recBuffer, pContent, bType)
+      if (pType < 0) return
 
-    const pType = bipf.seekKey(recBuffer, pContent, bType)
-    if (!~pType) return batch.length
+      if (bipf.compareString(recBuffer, pType, bContact) === 0) {
+        const content = bipf.decode(recBuffer, pContent)
+        const to = content.contact
 
-    if (bipf.compareString(recBuffer, pType, bContact) === 0) {
-      const content = bipf.decode(recBuffer, pContent)
-      const to = content.contact
+        if (isFeed(author) && isFeed(to)) {
+          const value = content.blocking || content.flagged ? -1 :
+                content.following === true ? 1
+                : -2
 
-      if (isFeed(author) && isFeed(to)) {
-        const value = content.blocking || content.flagged ? -1 :
-              content.following === true ? 1
-              : -2
+          let updateFeeds = false
 
-        let updateFeeds = false
+          let fromIndex = this.feeds.indexOf(author)
+          if (fromIndex === -1) {
+            this.feeds.push(author)
+            fromIndex = this.feeds.length -1
+            updateFeeds = true
+          }
 
-        let fromIndex = feeds.indexOf(author)
-        if (fromIndex === -1) {
-          feeds.push(author)
-          fromIndex = feeds.length -1
-          updateFeeds = true
-        }
+          let toIndex = this.feeds.indexOf(to)
+          if (toIndex === -1) {
+            this.feeds.push(to)
+            toIndex = this.feeds.length -1
+            updateFeeds = true
+          }
 
-        let toIndex = feeds.indexOf(to)
-        if (toIndex === -1) {
-          feeds.push(to)
-          toIndex = feeds.length -1
-          updateFeeds = true
-        }
+          let fromValues = this.feedValues[fromIndex] || {}
+          fromValues[toIndex] = value
+          this.feedValues[fromIndex] = fromValues
 
-        let fromValues = feedValues[fromIndex] || {}
-        fromValues[toIndex] = value
-        feedValues[fromIndex] = fromValues
-
-        const batchValue = {
-          type: 'put',
-          key: fromIndex,
-          value: fromValues
-        }
-
-        let existingKeyIndex = batchKeys[fromIndex]
-        if (existingKeyIndex) {
-          batch[existingKeyIndex] = batchValue
-        }
-        else {
-          batch.push(batchValue)
-          batchKeys[fromIndex] = batch.length - 1
-        }
-
-        if (updateFeeds) {
-          const feedsValue = {
+          const batchValue = {
             type: 'put',
-            key: 'feeds',
-            value: feeds
+            key: fromIndex,
+            value: fromValues
           }
 
-          let existingFeedsIndex = batchKeys['feeds']
-          if (existingFeedsIndex) {
-            batch[existingFeedsIndex] = feedsValue
-          } else {
-            batch.push(feedsValue)
-            batchKeys['feeds'] = batch.length - 1
+          let existingKeyIndex = this.batchKeys[fromIndex]
+          if (existingKeyIndex) {
+            this.batch[existingKeyIndex] = batchValue
           }
+          else {
+            this.batch.push(batchValue)
+            this.batchKeys[fromIndex] = this.batch.length - 1
+          }
+
+          if (updateFeeds) {
+            const feedsValue = {
+              type: 'put',
+              key: 'feeds',
+              value: this.feeds
+            }
+
+            let existingFeedsIndex = this.batchKeys['feeds']
+            if (existingFeedsIndex) {
+              this.batch[existingFeedsIndex] = feedsValue
+            } else {
+              this.batch.push(feedsValue)
+              this.batchKeys['feeds'] = this.batch.length - 1
+            }
+          }
+
+          this.layer(author, to, value)
         }
-
-        layer(author, to, value)
       }
     }
 
-    return batch.length
-  }
+    onLoaded(cb) {
+      pull(
+        pl.read(this.level, {
+          valueEncoding: this.valueEncoding,
+          keys: true
+        }),
+        pull.collect((err, data) => {
+          if (err) return cb(err)
 
-  layer({})
-
-  function beforeIndexUpdate(cb) {
-    get((err, g) => {
-      layer(g)
-      cb()
-    })
-  }
-
-  function get(cb) {
-    pull(
-      pl.read(level, {
-        valueEncoding: jsonCodec,
-        keys: true
-      }),
-      pull.collect((err, data) => {
-        if (err) return cb(err)
-
-        for (let i = 0; i < data.length; ++i) {
-          if (data[i].key === 'feeds') {
-            feeds = data[i].value
-            break
-          }
-        }
-
-        let result = {}
-        for (let i = 0; i < data.length; ++i)
-        {
-          const relation = data[i]
-
-          if (relation.key !== '\x00' && relation.key !== 'feeds') {
-            const feedIndex = parseInt(relation.key, 10)
-            const feed = feeds[feedIndex]
-            const feedFollowStatus = result[feed] || {}
-            const feedIndexValues = feedValues[feedIndex] || {}
-
-            let valueKeys = Object.keys(relation.value)
-            for (var v = 0; v < valueKeys.length; ++v) {
-              const toIndex = valueKeys[v]
-              const to = feeds[toIndex]
-              const value = parseInt(relation.value[valueKeys[v]], 10)
-              feedIndexValues[toIndex] = feedFollowStatus[to] = value
+          for (let i = 0; i < data.length; ++i) {
+            if (data[i].key === 'feeds') {
+              this.feeds = data[i].value
+              break
             }
-
-            result[feed] = feedFollowStatus
-            feedValues[feedIndex] = feedIndexValues
           }
-        }
 
-        cb(null, result)
-      })
-    )
-  }
+          let result = {}
+          for (let i = 0; i < data.length; ++i)
+          {
+            const relation = data[i]
 
-  sbot.db.registerIndex(() => {
-    return {
-      offset,
-      stateLoaded,
-      onData,
-      writeBatch,
-      name,
+            if (relation.key !== '\x00' && relation.key !== 'feeds') {
+              const feedIndex = parseInt(relation.key, 10)
+              const feed = this.feeds[feedIndex]
+              const feedFollowStatus = result[feed] || {}
+              const feedIndexValues = this.feedValues[feedIndex] || {}
 
-      remove: level.clear,
-      close: level.close.bind(level)
+              let valueKeys = Object.keys(relation.value)
+              for (var v = 0; v < valueKeys.length; ++v) {
+                const toIndex = valueKeys[v]
+                const to = this.feeds[toIndex]
+                const value = parseInt(relation.value[valueKeys[v]], 10)
+                feedIndexValues[toIndex] = feedFollowStatus[to] = value
+              }
+
+              result[feed] = feedFollowStatus
+              this.feedValues[feedIndex] = feedIndexValues
+            }
+          }
+
+          this.layer(result)
+          cb()
+        })
+      )
     }
-  })
+  }
 }
