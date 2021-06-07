ee6df464f3e1814a7b68f543317e0bb0a8e61d3d
Andre Staltz <andre@staltz.com>
2021-06-07 16:32:13 +0300

    rewrite glue/replicate.js to simplify it

--- glue/replicate.js
+++ glue/replicate.js
@@ -1,46 +1,27 @@
-const isFeed = require('ssb-ref').isFeed
 const pull = require('pull-stream')
 
-module.exports = function (sbot, layered, legacy) {
+module.exports = function (sbot, layered) {
   // check for ssb-replicate or similar, but with a delay so other plugins have time to be loaded
   setImmediate(function () {
     if (!sbot.replicate) {
       throw new Error('ssb-friends expects a replicate plugin to be available')
     }
 
+    const request = (sbot.replicate.request)
+    const block = (sbot.replicate.block) || (sbot.ebt && sbot.ebt.block)
+
     // opinion: replicate with everyone within max hops (max passed to layered above ^)
     pull(
       layered.hopStream({ live: true, old: true }),
-      pull.drain(function (data) {
-        if (data.sync) return
-        for (const k in data) {
-          sbot.replicate.request(k, data[k] >= 0)
+      pull.drain((hopsData) => {
+        if (hopsData.sync) return
+        for (const feedId of Object.keys(hopsData)) {
+          const val = hopsData[feedId]
+          if (val >= 0) request(feedId, true)
+          if (val === -1) block(sbot.id, feedId, true)
+          if (val === -2) block(sbot.id, feedId, false)
         }
       })
     )
-
-    // opinion: pass the blocks to replicate.block
-    const block = (sbot.replicate && sbot.replicate.block) || (sbot.ebt && sbot.ebt.block)
-    if (block) {
-      function handleBlockUnlock (from, to, value) {
-        if (value === false) block(from, to, true)
-        else block(from, to, false)
-      }
-      pull(
-        legacy.stream({ live: true }),
-        pull.drain(function (contacts) {
-          if (!contacts) return
-
-          if (isFeed(contacts.from) && isFeed(contacts.to)) { // live data
-            handleBlockUnlock(contacts.from, contacts.to, contacts.value)
-          } else { // initial data
-            for (const from in contacts) {
-              const relations = contacts[from]
-              for (const to in relations) { handleBlockUnlock(from, to, relations[to]) }
-            }
-          }
-        })
-      )
-    }
   })
 }
