fc0a24585f0502f0f2327bd7bd0ffc3b898b5c89
Andre Staltz <andre@staltz.com>
2021-06-09 16:50:19 +0300

    fix glue/replicate logic

--- glue/replicate.js
+++ glue/replicate.js
@@ -1,25 +1,29 @@
 const pull = require('pull-stream')
 
-module.exports = function replicationGlue(sbot, layered) {
+module.exports = function replicationGlue(sbot, layered, legacy) {
   // check for ssb-replicate or similar, but with a delay so other plugins have time to be loaded
   setImmediate(() => {
     if (!sbot.replicate) {
       throw new Error('ssb-friends expects a replicate plugin to be available')
     }
 
-    const request = (sbot.replicate.request)
-    const block = (sbot.replicate.block) || (sbot.ebt && sbot.ebt.block)
+    function updateEdge(orig, dest, value) {
+      if (orig === sbot.id) sbot.replicate.request(dest, !!value)
+      if (dest !== sbot.id) sbot.replicate.block(orig, dest, value === false)
+    }
 
-    // opinion: replicate with everyone within max hops (max passed to layered above ^)
     pull(
-      layered.hopStream({ live: true, old: true }),
-      pull.drain((hopsData) => {
-        if (hopsData.sync) return
-        for (const feedId of Object.keys(hopsData)) {
-          const val = hopsData[feedId]
-          if (val >= 0) request(feedId, true)
-          if (val === -1) block(sbot.id, feedId, true)
-          if (val === -2) block(sbot.id, feedId, false)
+      legacy.stream({ live: true }),
+      pull.filter(contacts => !!contacts),
+      pull.drain((contacts) => {
+        if (contacts.from && contacts.to) {
+          updateEdge(contacts.from, contacts.to, contacts.value)
+        } else {
+          for (const from of Object.keys(contacts)) {
+            for (const to of Object.keys(contacts[from])) {
+              updateEdge(from, to, contacts[from][to])
+            }
+          }
         }
       })
     )
