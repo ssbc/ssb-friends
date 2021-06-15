8ff993f55e905cf37f492af3451f046250ff98db
Andre Staltz <andre@staltz.com>
2021-06-15 18:12:40 +0300

    move some code into auth-glue.js

--- auth-glue.js
+++ auth-glue.js
@@ -1,5 +1,15 @@
-module.exports = function authGlue(sbot, isBlocking) {
-  // opinion: do not authorize peers blocked by this node.
+module.exports = function authGlue(sbot, layered, isBlocking) {
+  // Whenever we create a new block, immediately disconnect from peers we just
+  // blocked, if they are connected at all
+  layered.onEdge((orig, dest, value) => {
+    // if WE are BLOCKING a CONNECTED PEER
+    if (orig === sbot.id && value === -1 && sbot.peers[dest]) {
+      sbot.peers[dest].forEach(rpc => rpc.close(true))
+      sbot.peers[dest] = []
+    }
+  })
+
+  // Blocked peers also cannot *initiate* new connections
   sbot.auth.hook(function (fn, args) {
     const self = this
     const [feedId, cb] = args
