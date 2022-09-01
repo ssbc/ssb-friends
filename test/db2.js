7c4979ee27565ef4b13fb99f5f606170a227aab2
Andre Staltz <andre@staltz.com>
2022-09-01 12:17:31 +0300

    test that social graph is reset after compact

--- test/db2.js
+++ test/db2.js
@@ -4,7 +4,6 @@ const path = require('path')
 const ssbKeys = require('ssb-keys')
 const run = require('promisify-tuple')
 const pull = require('pull-stream')
-const validate = require('ssb-validate')
 const rimraf = require('rimraf')
 const mkdirp = require('mkdirp')
 const SecretStack = require('secret-stack')
@@ -39,17 +38,6 @@ function Server (opts = {}) {
   return stack(opts)
 }
 
-let state = validate.initial()
-
-function addMsg (db, keys, content) {
-  state = validate.appendNew(state, null, keys, content, Date.now())
-
-  return run((cb) => {
-    const value = state.queue.shift().value
-    db.add(value, cb)
-  })()
-}
-
 tape('db2 friends test', async (t) => {
   const alice = ssbKeys.generate()
   const bob = ssbKeys.generate()
@@ -68,21 +56,21 @@ tape('db2 friends test', async (t) => {
 
   await Promise.all([
     // Publish some irrelevant messages to test that the db2 index doesn't crash
-    addMsg(sbot.db, alice, { type: 'post', text: 'hello world' }),
-    addMsg(sbot.db, alice, { type: 'contact', contact: 'not a feed' }),
+    run(sbot.db.create)({keys: alice, content: { type: 'post', text: 'hello world' }}),
+    run(sbot.db.create)({keys: alice, content: { type: 'contact', contact: 'not a feed' }}),
     // Publish actual follows
-    addMsg(sbot.db, alice, u.follow(bob.id)),
-    addMsg(sbot.db, alice, u.follow(carol.id)),
-    addMsg(sbot.db, alice, u.follow(alice.id)),
-    addMsg(sbot.db, bob, u.follow(alice.id)),
-    addMsg(sbot.db, bob, {
+    run(sbot.db.create)({keys: alice, content: u.follow(bob.id)}),
+    run(sbot.db.create)({keys: alice, content: u.follow(carol.id)}),
+    run(sbot.db.create)({keys: alice, content: u.follow(alice.id)}),
+    run(sbot.db.create)({keys: bob, content: u.follow(alice.id)}),
+    run(sbot.db.create)({keys: bob, content: {
       type: 'contact',
       contact: carol.id,
       following: false,
       flagged: true
-    }),
-    addMsg(sbot.db, bob, u.block(david.id)),
-    addMsg(sbot.db, carol, u.follow(alice.id))
+    }}),
+    run(sbot.db.create)({keys: bob, content: u.block(david.id)}),
+    run(sbot.db.create)({keys: carol, content: u.follow(alice.id)}),
   ])
 
   const [err, hops] = await run(sbot.friends.hops)()
@@ -106,9 +94,9 @@ tape('db2 friends test', async (t) => {
   })
   live = liveHops(sbot)
 
-  const [err2] = await addMsg(sbot.db, alice, u.unfollow(carol.id))
+  const [err2] = await run(sbot.db.create)({keys: alice, content: u.unfollow(carol.id)})
   t.error(err2)
-  const [err3] = await addMsg(sbot.db, bob, u.follow(carol.id))
+  const [err3] = await run(sbot.db.create)({keys: bob, content: u.follow(carol.id)})
   t.error(err3)
 
   const [err4, hops2] = await run(sbot.friends.hops)()
@@ -144,10 +132,10 @@ tape('db2 unfollow', async (t) => {
   let live = liveHops(sbot)
 
   await Promise.all([
-    addMsg(sbot.db, alice, u.follow(bob.id)),
-    addMsg(sbot.db, alice, u.follow(carol.id)),
-    addMsg(sbot.db, bob, u.follow(alice.id)),
-    addMsg(sbot.db, carol, u.follow(alice.id))
+    run(sbot.db.create)({keys: alice, content: u.follow(bob.id)}),
+    run(sbot.db.create)({keys: alice, content: u.follow(carol.id)}),
+    run(sbot.db.create)({keys: bob, content: u.follow(alice.id)}),
+    run(sbot.db.create)({keys: carol, content: u.follow(alice.id)}),
   ])
 
   const [err, hops] = await run(sbot.friends.hops)()
@@ -165,7 +153,7 @@ tape('db2 unfollow', async (t) => {
   })
   live = liveHops(sbot)
 
-  const [err2] = await addMsg(sbot.db, alice, u.unfollow(bob.id))
+  const [err2] = await run(sbot.db.create)({keys: alice, content: u.unfollow(bob.id)})
   t.error(err2)
 
   const [err3, hops3] = await run(sbot.friends.hops)()
@@ -185,6 +173,56 @@ tape('db2 unfollow', async (t) => {
   const [err4, hopsAfter] = await run(sbot.friends.hops)()
   t.error(err4)
   t.deepEqual(hopsAfter, hops3)
+
+  await run(sbot.close)()
+  t.end()
+})
+
+tape('delete, compact, and reset social graph', async (t) => {
+  const alice = ssbKeys.generate()
+  const bob = ssbKeys.generate()
+  const carol = ssbKeys.generate()
+
+  rimraf.sync(dir)
+  mkdirp.sync(dir)
+
+  let sbot = Server({
+    keys: alice,
+    db2: true,
+    friends: {
+      hookAuth: false
+    },
+    path: dir
+  })
+
+  await Promise.all([
+    run(sbot.db.create)({keys: alice, content: u.follow(bob.id)}),
+    run(sbot.db.create)({keys: bob, content: u.follow(carol.id)}),
+  ])
+
+  const [err, hops] = await run(sbot.friends.hops)()
+  t.error(err)
+  t.deepEqual(hops, {
+    [alice.id]: 0,
+    [bob.id]: 1,
+    [carol.id]: 2
+  })
+
+  await run(sbot.db.deleteFeed)(bob.id)
+  t.pass("deleted bob's feed")
+
+  await run(sbot.db.compact)()
+  t.pass('compacted')
+
+  await run(sbot.db.onDrain)('contacts')
+  t.pass('reindexed contacts index')
+
+  const [err2, hops2] = await run(sbot.friends.hops)()
+  t.error(err2)
+  t.deepEqual(hops2, {
+    [alice.id]: 0,
+    [bob.id]: 1
+  })
 
   await run(sbot.close)()
   t.end()
